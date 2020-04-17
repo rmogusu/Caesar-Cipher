0d46e6874938db2bc359f34e76e4f987801e4ac4
rmogusu <rmogusu123@gmail>
2020-04-17 15:16:31 +0200

    Write coded code test to assert that my application can cipher upper case letters

--- src/test/java/models/CaesarCipherTest.java
+++ src/test/java/models/CaesarCipherTest.java
@@ -32,4 +32,18 @@ public class CaesarCipherTest {
         assertEquals("a", caesar.getCipher("z", 1));
         assertEquals("c", caesar.getCipher("x", 5));
     }
+    @Test
+    public void getCipher_shouldCipherUpperCaseLetter_cipherCharacters() {
+
+        CaesarCipher caesar =new CaesarCipher();
+        assertEquals("A", caesar.getCipher("A", 0));
+
+        assertEquals("B", caesar.getCipher("A", 1));
+        assertEquals("D", caesar.getCipher("A", 3));
+        assertEquals("J", caesar.getCipher("E", 5));
+
+        assertEquals("A", caesar.getCipher("Z", 1));
+        assertEquals("C", caesar.getCipher("X", 5));
+        assertEquals("Z", caesar.getCipher("C", -3));
+    }
 }
