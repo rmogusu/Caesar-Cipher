18eafb1f684dbf94c48eb9d554c3c3d22f3d3671
rmogusu <rmogusu123@gmail>
2020-04-17 14:59:47 +0200

    Added the smallest amount of logic to my first spec to make pass

--- src/main/java/models/CaesarCipher.java
+++ src/main/java/models/CaesarCipher.java
@@ -6,7 +6,7 @@ public class CaesarCipher {
     private static final int ALPHABET_SIZE = 26;
 
     public String getCipher(String message, int rotateBy) {
-    return null;
+    return "";
     }
 
 }
