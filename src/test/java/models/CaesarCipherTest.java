64f51b1b4f60697a8e679ff62b5c644396cc01ed
rmogusu <rmogusu123@gmail>
2020-04-17 15:14:13 +0200

    Write the coded test to assert that my  application can cipher lower case letters

--- src/test/java/models/CaesarCipherTest.java
+++ src/test/java/models/CaesarCipherTest.java
@@ -20,4 +20,16 @@ public class CaesarCipherTest {
         String symbols = "1!@#$%^&*(){}/";
         assertEquals(symbols, caesar.getCipher(symbols, 3));
     }
+    @Test
+    public void getCipher_shouldCipherLowerCaseLetter_cipherCharacters() {
+        CaesarCipher caesar = new CaesarCipher();
+        assertEquals("a", caesar.getCipher("a", 0));
+
+        assertEquals("b", caesar.getCipher("a", 1));
+        assertEquals("d", caesar.getCipher("a", 3));
+        assertEquals("j", caesar.getCipher("e", 5));
+
+        assertEquals("a", caesar.getCipher("z", 1));
+        assertEquals("c", caesar.getCipher("x", 5));
+    }
 }
