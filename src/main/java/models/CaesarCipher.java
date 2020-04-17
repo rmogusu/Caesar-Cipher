a900659cc6af5fda5a89fe72408a0611b2e56e25
rmogusu <rmogusu123@gmail>
2020-04-17 15:21:20 +0200

    Add logic to make my cipher test to pass

--- src/main/java/models/CaesarCipher.java
+++ src/main/java/models/CaesarCipher.java
@@ -1,12 +1,27 @@
 package models;
 
 
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
 public class CaesarCipher {
 
     private static final int ALPHABET_SIZE = 26;
 
     public String getCipher(String message, int rotateBy) {
-    return "";
+        rotateBy %= ALPHABET_SIZE;
+        char[] chars = message.toCharArray();
+        rotate(chars, rotateBy);
+        return new String(chars);
     }
 
+//    private void rotate(char[] chars, int rotateBy) {
+//        for (int i = 0; i < chars.length; i++) {
+//            if (isLowerCase(chars[i])) {
+//                chars[i] = rotateChar(chars[i], rotateBy, 'a', 'z');
+//            } else if (isUpperCase(chars[i])) {
+//                chars[i] = rotateChar(chars[i], rotateBy, 'A', 'Z');
+//            }
+//        }
+//    }
 }
