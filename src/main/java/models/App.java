dc6ab57fcb056998b9849d2e1ede7b5e61227992
rmogusu <rmogusu123@gmail>
2020-04-19 20:01:47 +0200

    Add a buffered reader in order to be able to read user input

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -1,10 +1,14 @@
 package models;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main(String[] args) {
 
         boolean programRunning = true;
         while (programRunning) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         }
     }
 }
